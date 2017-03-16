215c274b9cbe9eceb9b641de8fcc58ae640553b2
yenqwerty <yenqwerty@gmail.com>
2017-03-16 10:16:36 +0300

    check images url

--- news/models.py
+++ news/models.py
@@ -1,8 +1,14 @@
 from django.db import models
 
 class News(models.Model):
-    title = models.CharField(max_length=20, default='')
+    title = models.CharField(max_length=50, default='')
     description = models.CharField(max_length=100, default='')
-    image = models.ImageField(upload_to='/media/news',height_field=100, width_field=150)
+    image = models.ImageField(upload_to='news')
     text = models.TextField()
     pub_date = models.DateTimeField('date published')
+
+
+    def __str__(self):
+        return self.title
+
+
--- news/views.py
+++ news/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
 
-def index(request):
+from .models import News
+
+def detail(request,iq):
     return render(request, 'home/index.html')
+
+def news(request):
+    posts = News.objects.all()
+    return render(request, 'news/index.html', {'posts':posts})
